37021bfc5e375d5c79dadc7b2bffc25d2de9d9be
wangyao <wangyongyao1989@163.com>
2018-05-15 17:55:23 +0800

    添加到布局中

--- app/src/main/java/com/wangyongyao/allpowerfulcanvas/MainActivity.java
+++ app/src/main/java/com/wangyongyao/allpowerfulcanvas/MainActivity.java
@@ -1,13 +1,45 @@
 package com.wangyongyao.allpowerfulcanvas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wangyongyao.allpowerfulcanvas.views.AllPowerfulView;
+import com.wangyongyao.allpowerfulcanvas.views.TwoFingerSlidingSrcollView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.all_powerful_view)
+    AllPowerfulView mAllPowerfulView;
+
+    @BindView(R.id.two_finger_sliding_scrollview)
+    TwoFingerSlidingSrcollView mTwoFingerSlidingScrollview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        initView();
+        initData();
+        initEvent();
+    }
+
+    private void initEvent() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
     }
 }
